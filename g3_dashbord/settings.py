ab3d758d3ca2b2080b619c158b64f76d20ae5d25
Vasu <vasu04111@gmail.com>
2016-12-30 18:24:34 +0530

    added slug field to models and integerated postgreysql database

--- g3_dashbord/settings.py
+++ g3_dashbord/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'g3_dashbord.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'abcd1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
