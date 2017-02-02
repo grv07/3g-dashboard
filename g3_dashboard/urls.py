db99fd9b078947a7098e9e6c0d6b012d2f76a9b7
gaurav <grvtyagi22@gmail.com>
2017-02-02 12:39:51 +0530

    redirect blank url to /admin

--- g3_dashboard/urls.py
+++ g3_dashboard/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/admin')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
